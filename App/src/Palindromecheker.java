fc5aa22634470f156ce62cf6f8e467ac4e838f1f
ghoshbabu19 <ag1018@srmist.edu.in>
2026-03-16 08:59:08 +0530

    UC11

--- App/src/Palindromecheker.java
+++ App/src/Palindromecheker.java
@@ -1,20 +1,46 @@
-//version - 1.0
-//author - ghoshbabu19
-public class Palindromecheker {
-    static void main() {
-        String appName = "Palindrome Checker Application";
-        String appVersion = "Version 1.0";
+import java.util.Stack;
 
-        System.out.println("=======================================");
-        System.out.println("Welcome to " + appName);
-        System.out.println(appVersion);
-        System.out.println("=======================================");
-        System.out.println("Application started successfully.");
-        System.out.println("Ready to proceed to palindrome checking...");
-        System.out.println("Exiting application...");
-    }
+public class UseCase11PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        main();
+
+        // Test string
+        String word = "racecar";
+
+        // Create PalindromeChecker object
+        PalindromeChecker checker = new PalindromeChecker();
+
+        // Check palindrome
+        boolean result = checker.checkPalindrome(word);
+
+        if (result) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
+}
+
+// PalindromeChecker class encapsulates palindrome logic
+class PalindromeChecker {
+
+    // Method to check palindrome using Stack
+    public boolean checkPalindrome(String input) {
+
+        // Internal stack to reverse characters
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        // Compare characters
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
